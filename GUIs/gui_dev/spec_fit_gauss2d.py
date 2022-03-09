f58065464cff75df1865b4b404db2228ff273c0b
lblogan14 <bliu8@ncsu.edu>
2022-03-09 16:10:05 -0500

    example qt widgets

--- GUIs/gui_dev/spec_fit_gauss2d.py
+++ GUIs/gui_dev/spec_fit_gauss2d.py
@@ -1,5 +1,5 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QDialog
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QDialog, QComboBox, QLineEdit
+from PyQt5.QtCore import Qt, pyqtSignal
 
 import matplotlib
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
@@ -7,17 +7,83 @@ from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 import numpy as np
 
+from IGM.rb_setline import read_line_list
+
 class Gaussfit_2d(QDialog):
+	send_linelist = pyqtSignal(object)
+	send_lineindex = pyqtSignal(int)
+
 	def __init__(self,wave, flux1d,error1d):
 		super().__init__()
 		layout = QVBoxLayout()
+		# sublayout for Guess z functions
+		lines_layout = QHBoxLayout()
+		
+		line_combo = QComboBox()
+		line_combo.setFixedWidth(120)
+		line_combo.addItems(['NONE', 'LBG', 'Gal', 'LLS', 'LLS Small', 'DLA', 'atom'])
+		line_combo.currentTextChanged.connect(self._linelist_changed)
+		
+		ion_combo = QComboBox()
+		ion_combo.setFixedWidth(150)
+		ion_combo.addItem('NONE')
+		ion_combo.setCurrentIndex(0)
+		ion_combo.currentIndexChanged.connect(self._index_changed)
+
+		z_guess = QLineEdit()
+		z_guess.setPlaceholderText('Guess z')
+
+		lines_layout.addWidget(line_combo)
+		lines_layout.addWidget(ion_combo)
+		lines_layout.addWidget(z_guess)
+
 		line1d = LineCanvas()
 		line1d._plot_hist(flux2d)
 		mpl_toolbar = NavigationToolbar(line1d, self)
+
+		# main layout
+		layout.addLayout(lines_layout)
 		layout.addWidget(mpl_toolbar)
 		layout.addWidget(line1d)
 		self.setLayout(layout)
 		self.setFixedSize(600,600)
+
+	def _index_changed(self, i): # i is an int
+		self.send_lineindex.emit(i)
+
+		#receiver should have
+
+	def _linelist_changed(self, s):
+		if s in 'NONE':
+			self.send_linelist.emit(s)
+			self.l_combobox.clear()
+			self.l_combobox.addItem('NONE')
+			self.l_combobox.setCurrentIndex(0)
+		else:
+			llist = self._get_linelist_df(s)
+			self.linelist = llist
+
+			self.l_combobox.addItems(['ALL'] + self.linelist['name'].tolist())
+			self.send_linelist.emit(self.linelist)
+			self.l_combobox.setCurrentIndex(1)
+
+	def _get_linelist_df(self, linelist_name):
+		llist = pd.DataFrame(columns=['wave', 'name'])
+		tmp = read_line_list(linelist_name)
+
+		#need a line to append wrest to name if it doesn't have one
+		if any(map(str.isdigit, tmp[1]['ion'])):
+			# if name column has wrest
+			for li in tmp:
+				newrow = {'wave': li['wrest'], 'name': li['ion']}
+				llist = llist.append(newrow, ignore_index=True)
+		else:
+			# if name column doesn't have wrest, need to append
+			for li in tmp:
+				newrow = {'wave': li['wrest'], 'name': li['ion']+' '+str(round(li['wrest']))}
+				llist = llist.append(newrow, ignore_index=True)
+
+		return llist
 
 class LineCanvas(FigureCanvasQTAgg):
 	def __init__(self, parent=None, width=5, height=3, dpi=100):
