2f42b755f6c183b4694ab40134d78bd87457c754
rongmon <rongmon.bordoloi@gmail.com>
2022-05-18 20:43:37 -0400

    new function added

---
+++ catalog/convert_FIRE_coordinates.py
@@ -0,0 +1,36 @@
+from astropy.coordinates import FK5, SkyCoord
+from astropy.time import Time
+from astropy import units as u
+from astropy.io import fits
+
+def convert_epoch(filename):
+	''''--------------------------------------------
+	Function to take Magellan FIRE spectrum and use the header information
+	to transform the co-ordinates to J2000 epoch
+
+	Input:  Magellan FIRE 1d spectrum filename in fits format
+
+	Return:
+	      astropy SkyCoord object in J2000 epoch
+
+
+
+	Note: Only for specific use of older Magellan FIRE QSO spectra.
+
+
+	Written By: Rongmon Bordoloi              May 2022
+       ---------------------------------------------
+	'''
+	hdu=fits.open(filename)
+	equinox     =  hdu[0].header['EQUINOX']
+	ra_deg1      =  hdu[0].header['RA']
+	dec_deg1     =  hdu[0].header['DEC']
+
+	fk5_obs = FK5(equinox=Time(equinox, format='jyear'))
+	fk5_2000 = FK5(equinox=Time(2000.0, format='jyear'))
+	c = SkyCoord(ra_deg1,dec_deg1,frame=fk5_obs,unit=(u.deg,u.deg))
+	c_j2000 = c.transform_to(fk5_2000)
+	#ra_deg = c_j2000.ra.deg
+	#dec_deg = c_j2000.dec.deg
+
+	return c_j2000
